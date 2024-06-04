584bb30694e559852c26a38d3f3e50dac939d8d5
yunsungjoong <yun_sj94@naver.com>
2024-06-04 23:25:32 +0900

    수정

--- src/data/todos.ts
+++ src/data/todos.ts
@@ -1,20 +1,20 @@
 export default [
     {
         id: 1,
-        title: '테니스치기',
-        description:'포핸드마스터',
+        title: '런데이앱 하루 5km 달리기',
+        description:'물 챙기기 , 40분 뛰기 ',
         complete:false
     },
     {
         id: 2,
-        title: 'sat공부',
-        description:'존나어려워',
+        title: 'ionic 과제 준비',
+        description:'TodoList 작업',
         complete:false
     },
     {
         id: 3,
-        title: '테ㅇㄹ',
-        description:'백핸드마스터',
+        title: '수요일 과제제출',
+        description:'안드로이드 apk, 간략한 소스',
         complete: true
     }
 ]
