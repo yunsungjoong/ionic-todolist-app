8dfb081a2ae53750f91582ec384a4a87b19a96ed
yunsungjoong <yun_sj94@naver.com>
2024-06-04 20:19:14 +0900

    수정

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        title: '테니스치기',
+        description:'포핸드마스터',
+        complete:false
+    },
+    {
+        id: 2,
+        title: 'sat공부',
+        description:'존나어려워',
+        complete:false
+    },
+    {
+        id: 3,
+        title: '테ㅇㄹ',
+        description:'백핸드마스터',
+        complete: true
+    }
+]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import todos from '../../data/todos';
+//todos.ts에 있는 todos 데이터를 import시킨다.
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  todos:{
+    id: number,
+    title: string,
+    description: string,
+    complete: boolean
+  }[];
+  //위와 같은 배열은 객체나 인터페이스 선언해서 쓰는게 좋다. 여기서는 그냥 인라인에서 하자.
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit(){
+    this.todos = todos;
+    //import해온 세개의 데이터를 바로 위에 클래스 위에 있는, todos에 넣는다.
   }
 
 }
